30f483cd457bcc45c1b6bcebd2af45ec561d2d6c
sampathpanem <sampath.panem@gmail.com>
2025-11-30 19:02:07 +0530

    Header section css and footer section

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+    
+
+ 
+    <footer>
+      <div className="footer-container">
+
+        <h2 className="footer-brand">FashionHub</h2>
+
+        <ul className="footer-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+
+      </div>
+
+      <p className="footer-copy">
+        © {new Date().getFullYear()} FashionHub. All rights reserved.
+      </p>
+    </footer>
+
+
+
+      
+
+  
+      
+    
+    </div>
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,18 +4,20 @@ const Header = () => {
   return (
     <div className='headerSection'>
       <div className="left">
-       <div > shopping</div>
+       <div > Fashionhub</div>
       </div>
       <div className="center">
         <ul>
-          <li>women</li>
-          <li>men</li>
-          <li>kids</li>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">Women</a></li>
+          <li><a href="#">Kids</a></li>
+          
         </ul>
       </div>
-      <div className="search"> 
-      <input type="text" placeholder='Search'></input>
-      </div>
+      <div className="search-wrapper">
+  <input type="text" placeholder="Search..." className="search-input" />
+  <button className="search-btn">Search</button>
+</div>
       <div className="right">
         <div className="signin">
           Signin/SignUp
